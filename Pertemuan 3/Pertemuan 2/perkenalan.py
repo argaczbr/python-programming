8bd13e0844e74dd388b2ca28fb2d184d853fd1f1
argaczbr <brahmantiaarga@gmail.com>
2024-07-30 09:40:23 +0700

    Add files via upload

---
+++ Pertemuan 3/Pertemuan 2/perkenalan.py
@@ -0,0 +1,7 @@
+nama = "Arga Czar Brahmantya"
+kelas = "TKJ X A"
+hobi = "Dengerin Musik"
+
+print("Perkenalkan nama saya ", nama)
+print(f"Saya kelas {kelas}")
+print(f"hobi saya {hobi}, saya sangat senang melakukannya")
---
+++ Pertemuan 3/aritmatika.py
@@ -0,0 +1,16 @@
+bil1 = 8
+bil2 = 2
+
+print("Penjumlahan :", bil1 + bil2)
+
+print("Pengurangan :", bil1 - bil2)
+
+print("Perkalian :", bil1 * bil2)
+
+print("Pembagian :", bil1 / bil2)
+
+print("Perpangkatan :", bil1 ** bil2)
+
+print("modulus :", bil1 % bil2)
+
+print("Pembagian bulat :", bil1 // bil2)
