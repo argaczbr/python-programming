cbd1adf49a2e75b95098cb6e01194ff864259923
argaczbr <brahmantiaarga@gmail.com>
2024-08-01 12:11:23 +0700

    Add files via upload

---
+++ Pertemuan 4/penugasan.py
@@ -0,0 +1,25 @@
+x = 7
+
+#penjumlahan 
+x += 5
+print("Penjumlahan: ", x)
+
+#pengurangan
+x -= 2
+print("Pengurangan: ", x)
+
+#perkalian 
+x *= 7
+print("Perkalian: ", x)
+
+#pembagian
+x /= 5
+print("Pembagian: ", x)
+
+#modulus
+x %= 4
+print("Modulus: ", x)
+
+#perpangkatan
+x **= 16
+print("Pangkat: ", x)
