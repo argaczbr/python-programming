b07b2f8db2827bcb749a97d1f8a57f2aa57273db
argaczbr <brahmantiaarga@gmail.com>
2024-08-27 09:45:42 +0700

    Add files via upload

---
+++ Latihan1.py
@@ -0,0 +1,14 @@
+# Meminta input
+tugas1 = 98
+tugas2 = 69
+tugas3 = 57
+
+nilai_tugas1 = float(input("Masukkan nilai tugas 1:"))
+nilai_tugas2 = float(input("Masukkan nilai tugas 2:"))
+nilai_tugas3 = float(input("Masukkan nilai tugas 3:"))
+
+# Menghitung rata-rata
+rata_rata = (nilai_tugas1 + nilai_tugas2 + nilai_tugas3) / 3
+
+# Menampilkan hasil
+print("Rata-rata nilai tugas adalah:", rata_rata)
